631fe0bc4ea513a81f84e497b7b732d256191f5b
wangjinrui <wangjinrui@hongqitech.com>
2018-03-01 13:12:34 +0800

    创建NFC测试项目

---
+++ JRNFCTest/JRNFCTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JRNFCTest
+//
+//  Created by hqtech on 2018/3/1.
+//  Copyright © 2018年 tulip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JRNFCTest/JRNFCTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JRNFCTest
+//
+//  Created by hqtech on 2018/3/1.
+//  Copyright © 2018年 tulip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
